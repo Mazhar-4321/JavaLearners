2461218f2f706f865110920a36f6df0603ef9fc7
Mazhar-4321 <syedmazharali742@gmail.com>
2022-12-06 13:16:51 +0530

    ADDED: 06 Dec Class and Topics are: Classes And Objects

---
+++ src/package06Dec22/Employee.java
@@ -0,0 +1,31 @@
+package package06Dec22;
+
+public class Employee {
+    private String employeeName;
+    private int salary;
+    private String empId;
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+}
---
+++ src/package06Dec22/Student.java
@@ -0,0 +1,31 @@
+package package06Dec22;
+
+public class Student {
+    private String name;
+    private String collegeName;
+    private String mobileNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCollegeName() {
+        return collegeName;
+    }
+
+    public void setCollegeName(String collegeName) {
+        this.collegeName = collegeName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+}
