c04a9bebeaf8cf6b5232c1e5c6d6df570030bd21
Mazhar-4321 <syedmazharali742@gmail.com>
2022-12-07 13:40:53 +0530

    ADDED: Code to take input from Scanner

--- src/Dec_07_2022/Main.java
+++ src/Dec_07_2022/Main.java
@@ -1,7 +1,13 @@
 package Dec_07_2022;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Addition Program");
+        System.out.println("Enter Two Numbers");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
     }
 }
